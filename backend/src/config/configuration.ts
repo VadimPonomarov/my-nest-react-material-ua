56dc9495d31244c2e19dc40661af440eeb3ba9b8
pvsve <pvs.versia@gmail.com>
2023-03-07 22:19:40 +0200

    docker-compose stable

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -17,7 +17,7 @@ const swagger = new DocumentBuilder()
     } ,'access-token')
     .build();
 export default () => ({
-    port: parseInt(process.env.PORT, 10) || 3001,
+    port: parseInt(process.env.SERVER_PORT, 10) || 3001,
     database: {
         host: process.env.DATABASE_HOST,
         port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
